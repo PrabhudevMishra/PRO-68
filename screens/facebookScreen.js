e3fc9c2685be6480eff8f452d3d25b4b3ef6f4df
PrabhudevMishra <70210156+PrabhudevMishra@users.noreply.github.com>
2021-01-20 07:56:22 +0530

    Add files via upload

---
+++ screens/facebookScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import{View,Text} from 'react-native';
+export default class FacebookScreen extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1,justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Facebook</Text>
+            </View>
+        )
+    }
+}
